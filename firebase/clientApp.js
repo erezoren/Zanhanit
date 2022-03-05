2ea88b63952793b5eaf3697504dea315a9b00d5e
eoren <eoren@ebay.com>
2022-03-05 22:49:50 +0200

    set storageBucket

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -7,7 +7,7 @@ const clientCredentials = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: 'zanhanit',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
