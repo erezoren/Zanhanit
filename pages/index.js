82a6eda664e2585f90ebe1b34106ae485ee0e899
eoren <eoren@ebay.com>
2022-03-05 12:35:55 +0200

    home page css

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import homeStyle from '../styles/home-page.module.css'
 import {Header} from "../components/Header";
 
 export default function Home(props) {
   return (
-    <div className={styles.container}>
-      <Header/>
-    </div>
+      <div className={styles.container}>
+        <Header/>
+        <div className={styles.container} dir="rtl">
+          <span className={homeStyle.welcomeTitle}>פאב הצנחנית להבות חביבה</span>
+        </div>
+      </div>
   )
 }
