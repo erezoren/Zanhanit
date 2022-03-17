938db2d4e83681f304c574e0e0622b8c95d090fb
eoren <eoren@ebay.com>
2022-03-17 12:19:43 +0200

    protect profile

--- pages/admin.js
+++ pages/admin.js
@@ -2,12 +2,15 @@ import styles from '../styles/Home.module.css'
 import {Header} from "../components/common/Header";
 import Profile from "../components/common/Profile";
 import Link from 'next/link'
+import {useUser} from "@auth0/nextjs-auth0";
 
 export default function Admin(props) {
+  const {user} = useUser();
+
   return (
       <div className={styles.container}>
         <Header/>
-        <Profile/>
+        {user && <Profile/>}
         <h3 className="title">
           Read{' '}
           <Link href="/api/auth/login">
