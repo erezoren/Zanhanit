4350e3a7748e32ccd69d7b996071114784fbbbd6
eoren <eoren@ebay.com>
2022-03-17 11:41:49 +0200

    changed to next/link

--- pages/admin.js
+++ pages/admin.js
@@ -1,15 +1,25 @@
 import styles from '../styles/Home.module.css'
 import {Header} from "../components/common/Header";
 import Profile from "../components/common/Profile";
+import Link from 'next/link'
 
 export default function Admin(props) {
   return (
       <div className={styles.container}>
         <Header/>
         <Profile/>
-        <a href="/api/auth/login">Login</a>
-        <br/>
-        <a href="/api/auth/logout">Logout</a>
+        <h3 className="title">
+          Read{' '}
+          <Link href="/api/auth/login">
+            <a>Login</a>
+          </Link>
+        </h3>
+        <h3 className="title">
+          Read{' '}
+          <Link href="/api/auth/logout">
+            <a>Logout</a>
+          </Link>
+        </h3>
         <div className={styles.container} dir="rtl">
           <p>הוספת אירוע</p>
         </div>
