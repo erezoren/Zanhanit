541c1bc4199e7a280d94db2bec5aba893f65393f
eoren <eoren@ebay.com>
2022-03-25 01:04:19 +0200

    env prod

--- pages/barmens.js
+++ pages/barmens.js
@@ -1,13 +1,7 @@
 import styles from '../styles/Home.module.css'
 import homeStyle from '../styles/home-page.module.css'
 import {Header} from "../components/common/Header";
-import {DatesDropdown} from "../components/tickets/DatesDropdown";
-import {dateStamp} from "../lib/common_utils";
-import {getAllEventDates, getEvents} from "../lib/eventsHandler";
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {EventsDisplay} from "../components/events/EventsDisplay";
-import {getAllBarmenDates, getBarmens} from "../lib/barmensHandler";
+import {getBarmens} from "../lib/barmensHandler";
 
 export async function getServerSideProps(context) {
   return {
