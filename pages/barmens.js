5be8de1d8246c8322d851159f798d667d5264db0
eoren <eoren@ebay.com>
2022-03-25 03:35:20 +0300

    barmens view

--- pages/barmens.js
+++ pages/barmens.js
@@ -13,13 +13,24 @@ export async function getServerSideProps(context) {
 
 export default function Barmens(props) {
   const {barmens} = props;
-
+debugger
   return (
       <div className={styles.container}>
         <Header/>
         <div className={styles.container} dir="rtl">
           <span className={homeStyle.welcomeTitle}><h3 className="display-1">ברמנים/יות</h3></span>
-
+          {
+            barmens.map(bm=>{
+              return <div>
+                <p>{bm.date}</p>
+              <ul>
+                {bm.barmens.map(name=>{
+                  return <li>{name}</li>
+                })}
+              </ul>
+              </div>
+            })
+          }
           <div>
           </div>
 
